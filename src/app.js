12d2f02f0bfc7e02b3af000d4cc068a74324b1b8
Braine Lomoni <kapolonbraine@gmail.com>
2024-01-13 15:58:40 +0300

    fix : Allow cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 import express from "express";
 import fplRouter from "./api/fpl/index.js"
-import swaggerJsDocs from "swagger-jsdoc"
-import swaggerUI from "swagger-ui-express"
+import cors from "cors"
 export const app = express();
 
-// app.use(express.json)
+// MIDDLEWARE
+app.use(cors())
 app.use(fplRouter)
 
 app.listen(6969,()=>{
