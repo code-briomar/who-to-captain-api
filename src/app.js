8d0c86e0c52fcee25d865cc374e5a03bf93a5eff
Braine Lomoni <kapolonbraine@gmail.com>
2024-01-17 19:39:23 +0300

    feature : Pass over data on whether a player has been benched or not

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 import express from "express";
+import https from "https";
 import fplRouter from "./api/fpl/index.js"
 import cors from "cors"
+import axios from "axios";
 export const app = express();
 
 // MIDDLEWARE
 app.use(cors())
 app.use(fplRouter)
 
-app.listen(6969,()=>{
+app.listen(6969, () => {
     console.log("Listening on PORT 6969");
 })
